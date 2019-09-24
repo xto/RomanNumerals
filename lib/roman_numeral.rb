0c2fae7652ed405d1edf8f8cc5254a98c4129d4e
Xavier Tô <xavier.to@shopify.com>
2019-09-24 14:58:56 -0400

    initial commit

---
+++ lib/roman_numeral.rb
@@ -0,0 +1,2 @@
+class RomanNumeral
+end
---
+++ test/roman_numeral_test.rb
@@ -0,0 +1,8 @@
+require 'test/unit'
+require_relative '../lib/roman_numeral.rb'
+
+class RomanNumeralTest < Test::Unit::TestCase
+  def test_bogus
+    assert false
+  end
+end
